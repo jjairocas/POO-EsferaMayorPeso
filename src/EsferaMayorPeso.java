662f04765891d894232142c38de753f1bc757401
jjairocas <jjcastan@unal.edu.co>
2023-09-04 17:42:08 -0500

    Esferas de Mayor Peso

---
+++ src/EsferaMayorPeso.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class EsferaMayorPeso {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("ESFERA A:");
+        int PesoEsferaA= input.nextInt();
+        System.out.println("ESFERA B:");
+        int PesoEsferaB= input.nextInt();
+        System.out.println("ESFERA C:");
+        int PesoEsferaC= input.nextInt();
+
+        int EsferaMayor=0;
+
+        if((PesoEsferaA>PesoEsferaB) && (PesoEsferaA>PesoEsferaC)){
+            EsferaMayor=PesoEsferaA;
+            System.out.println("LA ESFERA DE MAYOR PESO ES LA ESFERA A DE:"+EsferaMayor+"gr");
+        }
+        else if((PesoEsferaB>PesoEsferaA) && (PesoEsferaB>PesoEsferaC)){
+            EsferaMayor=PesoEsferaB;
+            System.out.println("LA ESFERA DE MAYOR PESO ES LA ESFERA B DE:"+EsferaMayor+"gr");
+        }
+        else if((PesoEsferaC>PesoEsferaA) && (PesoEsferaC>PesoEsferaB)){
+            EsferaMayor=PesoEsferaC;
+            System.out.println("LA ESFERA DE MAYOR PESO ES LA ESFERA C DE: "+EsferaMayor+"gr");
+        }
+
+    }
+}
